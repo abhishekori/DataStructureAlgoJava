c0f7741351df2c64f6d19ba94181885de7940f5d
abhishek <abhikori1994@gmail.com>
2016-08-10 14:34:55 +0530

    get the number of nodes

--- gfg/LinkList.java
+++ gfg/LinkList.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 class  LinkList{
 	Node head; // Head is the pointer which points to the first element in the list
 
 	static class Node{ // This class represents one node in the link list
 		int data; // data in each node
-		Node next; // next pointer posints to the next element 
+		Node next; // next pointer posints to the next element
 
-		public Node(int d){ // Prameterised contructor which inilizes the node 
-			data=d; 
+		public Node(int d){ // Prameterised contructor which inilizes the node
+			data=d;
 			next=null;
 		}
 
@@ -58,16 +59,16 @@ class  LinkList{
 
 		Node new_node = new Node(new_data); // create a node
 		new_node.next=prev_node.next; // point new node next to prev node next
-		prev_node.next=new_node; // point prev node next to new node 
+		prev_node.next=new_node; // point prev node next to new node
 
 	}
 
 	void insertNodeAtLast(int new_data){
 
-		Node new_node = new Node(new_data); // create new node 
+		Node new_node = new Node(new_data); // create new node
 		new_node.next=null; // point the next of new node to null as it ll be the last node
 
-		Node temp=this.head; 
+		Node temp=this.head;
 
 		while(temp.next!=null){ // loop thorugh till you reach the last node
 			temp=temp.next;
@@ -109,6 +110,20 @@ class  LinkList{
 
 	}
 
+	public int getCount(){
+
+		int count=0;
+		Node h=this.head;
+
+		while(h!=null){
+			count++;
+			h=h.next;
+		}
+
+		return count;
+
+	}
+
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		int option;
@@ -120,7 +135,7 @@ class  LinkList{
 		Node second = new Node(2); // create 2nd node
 		Node third = new Node(3); // create third node
 
-		list.head = first; // connecting head to first node 
+		list.head = first; // connecting head to first node
 
 		first.next=second; // connecting first.next pointer to second node
 		second.next=third; // connecting second.next pointer to third node
@@ -131,7 +146,7 @@ class  LinkList{
 
 		 System.out.println("the got node is "+test.data);
 
-		System.out.println("Please choose the option \n 1. insert at begening \n 2. Insert in after a node \n 3. Insert at the last \n 4. Delete a node \n 5, delete node by index");
+		System.out.println("Please choose the option \n 1. insert at begening \n 2. Insert in after a node \n 3. Insert at the last \n 4. Delete a node \n 5, delete node by index \n 6. Count the number of nodes");
 		option = s.nextInt();
 
 		switch(option){
@@ -161,7 +176,7 @@ class  LinkList{
 			break;
 
 			case 2:
- 
+
 			int prev_num;
 			System.out.println("Please enter the number to insert");
 			System.out.print("Insert an element? (y/n) :");
@@ -214,7 +229,7 @@ class  LinkList{
 
 			break;
 
-			
+
 
 			case 4:
 
@@ -265,6 +280,10 @@ class  LinkList{
 			  System.out.println("Thank you");
 
 			break;
+
+			case 6:
+				System.out.println(list.getCount());
+				break;
 
 			default: System.out.print("Wrong option");
 
